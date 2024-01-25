1673690a4b65dbb1807e9d25b6a683db43356ad3
kristinashinkarenco <squirrelonair@gmail.com>
2024-01-25 13:54:19 +0300

    25_01/4

---
+++ area_of_a_triangle.rb
@@ -0,0 +1,13 @@
+puts "Let's find the area of the triangle!
+/n
+You only need to know the height and base of the triangle. "
+
+print "What is the base of the triangle? "
+base=gets.chomp
+
+print "What is the height of the triangle? "
+height=gets.chomp
+
+area= (base*height)/2.0
+
+puts "Area of triangle is #{area} square units!"
