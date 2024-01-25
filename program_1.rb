f4264207583eedc131bed04bbee6f5a047a78755
kristinashinkarenco <squirrelonair@gmail.com>
2024-01-25 13:13:48 +0300

    25_01/3

--- program_1.rb
+++ program_1.rb
@@ -1,5 +1,28 @@
 # Приветствие
-print "Hello! What's your name? "
-name = gets.chomp
-name.capitalize! 
-print "Welcome, #{name}! "
+print "Hello! What's your first name? "
+first_name = gets.chomp
+first_name.capitalize!
+print "Welcome, #{first_name}! "
+
+#Основная часть
+print "Write your height: "
+height = gets.chomp
+
+print "Write your weight: "
+weight = gets.chomp
+
+estimated_weight = (height-110)*1.15
+
+#результат 
+print "#{first_name} your ideal weight is #{estimated_weight}! "
+
+if 
+	weight-estimated_weight < 0
+	print "Great! You are already at your ideal weight! "
+
+if 
+	weight-estimated_weight >= 0.0
+	print "Great job, you're on the right way! "
+
+end
+
