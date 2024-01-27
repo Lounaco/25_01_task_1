95d3055a5245973a232b60d0b87d464e4192314a
kristinashinkarenco <squirrelonair@gmail.com>
2024-01-27 20:17:27 +0300

    27_01/1

--- program_3.rb
+++ program_3.rb
@@ -1,10 +1,26 @@
-print "Write  "
-a = gets.to_i
+#task conditions and variables
+puts "Let's determine the type of triangle? "
+
+print "Write the first side of the triangle. "
+a = gets.chomp.to_f
 
 
-print "Write  "
-b = gets.to_i
+print "Write the second side of the triangle. "
+b = gets.chomp.to_f
 
 
-print "Write "
-c = gets.to_i
+print "Write the third side of the triangle. "
+c = gets.chomp.to_f
+
+#solution
+
+if a = b = c
+  puts "Equilateral triangle "
+
+elsif  (a == c) || (c == b) || (a == b) 
+  puts "Isosceles triangle "
+
+else (a**2 == c**2 + b**2) || (b**2 == c**2 + a**2) || (c**2 == b**2 + a**2)
+  puts "right triangle "
+
+end
